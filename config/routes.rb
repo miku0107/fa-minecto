321c870ef657cd8e117c21a0dd5a21bf31d27371
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-10-09 23:50:20 +0000

    User/Admin　サインアップ・ログイン機能作成（未テスト）

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  
+  #エンドユーザー
+  devise_for :users, skip: [:passwords], controllers:{
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  #アドミンユーザー
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers:{
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
