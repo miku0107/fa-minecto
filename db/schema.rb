110b931846886556008053a46f9cbb8f4b2f0a9b
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-10-16 18:47:02 +0000

    bioデフォルト値削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_13_203707) do
+ActiveRecord::Schema.define(version: 2023_10_16_184124) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 2023_10_13_203707) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.text "bio", default: ""
+    t.text "bio"
     t.date "birth_date"
     t.string "location", default: ""
     t.boolean "is_active", default: true, null: false
