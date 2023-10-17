4f7a1cc543b7f6d9272bfbc59a6c43eaa90d6444
miku0107 <138674231+miku0107@users.noreply.github.com>
2023-10-17 10:07:45 +0000

    User bio　デフォルト値削除

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 2023_10_13_203707) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.text "bio", default: ""
+    t.text "bio"
     t.date "birth_date"
     t.string "location", default: ""
     t.boolean "is_active", default: true, null: false
